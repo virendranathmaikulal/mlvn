7c51f7002d384088421f1ab32a598a314a09f3c9
Dipali Khandelwal <deepali.khandelwal19@gmail.com>
2026-03-01 17:03:16 +0530

    gcloud setup

---
+++ functions/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAfFduA4HLa9kCwTgKeBs6vkD0uCwrPMTc",
+  authDomain: "mlvn-374c5.firebaseapp.com",
+  projectId: "mlvn-374c5",
+  storageBucket: "mlvn-374c5.firebasestorage.app",
+  messagingSenderId: "749511175191",
+  appId: "1:749511175191:web:0b1018692cb8d8724b4052"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
