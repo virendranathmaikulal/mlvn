ee46e1394951b5bc2bc35d2b0383892ab84ebd9b
virendranathmaikulal <virendranathmaikulal@gmail.com>
2026-03-06 15:29:47 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             {/* Public routes without sidebar */}
-            <Route path="/" element={<Homepage />} />
+            <Route path="/" element={<Index />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             
